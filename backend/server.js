e9193ec535769692a9447d43e8495f708d5edfb1
SahilIjaz <hssahil2913@gmail.com>
2026-03-24 00:00:36 +0500

    kljlkjlk

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,6 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 // Static files for uploads
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
